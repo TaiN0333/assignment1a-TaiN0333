4e362536f4a5250f7dd56ddaae36df7a96c3cb04
Tai Nguyen <thn5094@psu.edu>
2024-09-09 18:55:13 -0400

    Hello

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "TaiN0333";
+    char STUDENT_ID[] = "938910333";
 
     return 0 ;
 }
